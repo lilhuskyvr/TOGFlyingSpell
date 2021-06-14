60f8b062dc8ea82d8b2f9bad79c8bfd6b26338a6
Lil Husky VR <lilhuskyvr@gmail.com>
2021-06-14 18:58:19 +1000

    version 1.0

---
+++ TOGFlyingSpell/TOGFlyingSpellPatch.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using TOGModFramework;
+using TOGModFramework.Extensions;
+using UnityEngine;
+
+namespace TOGFlyingSpell
+{
+    public class TOGFlyingSpellPatch : MonoBehaviour
+    {
+        private bool _buttonJumpPressed;
+
+        public void Inject()
+        {
+            EventManager.OnPlayerLoaded += EventManagerOnOnPlayerLoaded;
+        }
+
+        private void EventManagerOnOnPlayerLoaded()
+        {
+            ConfigManager.local.StartCoroutine(PlayerControlInputListenerCoroutine());
+        }
+
+        private IEnumerator PlayerControlInputListenerCoroutine()
+        {
+            while (Player.local != null)
+            {
+                var currentButtonJumpPressed = Player.local.controlInput.OrderController.ButtonBPressed;
+
+                if (currentButtonJumpPressed)
+                {
+                    Player.local.rb.velocity = 15 * Player.local.Head.transform.up;
+                }
+
+                //stop pressing trigger
+                if (!currentButtonJumpPressed && _buttonJumpPressed)
+                {
+                    Player.local.rb.velocity = Vector3.zero;
+                    Player.local.isJumping = false;
+                }
+
+                //start pressing trigger
+                if (currentButtonJumpPressed && !_buttonJumpPressed)
+                {
+                    Player.local.rb.velocity = Vector3.zero;
+                    Player.local.isJumping = true;
+                }
+
+                if (Player.local.IsFlying())
+                {
+                    Player.local.rb.useGravity = false;
+                }
+                else
+                {
+                    Player.local.rb.useGravity = true;
+                }
+
+                _buttonJumpPressed = currentButtonJumpPressed;
+
+                yield return new WaitForFixedUpdate();
+            }
+
+            yield return null;
+        }
+    }
+}
